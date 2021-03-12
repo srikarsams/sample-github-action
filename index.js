5b82475988458f1e72619d718047435cd2e24e0d
Srikar Samudrala <srikarsams@gmail.com>
2021-03-12 14:51:52 +0530

    branch compare

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
+function doesPrNeedsUpdate(octokit, pr_data) {
+  if (pr_data.merged === true) {
+    core.warning("Skipping pull request, already merged.");
+    return false;
+  }
+  if (pr_data.state !== "open") {
+    core.warning(
+      `Skipping pull request, no longer open (current state: ${pull.state}).`
+    );
+    return false;
+  }
+  if (!pr_data.head.repo) {
+    core.warning(`Skipping pull request, fork appears to have been deleted.`);
+    return false;
+  }
+
+  const { data: comparison } = octokit.repos.compareCommits({
+    owner: pr_data.head.repo.owner.login,
+    repo: pr_data.head.repo.name,
+    // This base->head, head->base logic is intentional, we want
+    // to see what would happen if we merged the base into head not
+    // vice-versa.
+    base: pr_data.head.label,
+    head: pr_data.base.label,
+  });
+
+  if (comparison.behind_by === 0) {
+    core.info("Skipping pull request, up-to-date with base branch.");
+    return false;
+  }
+
+  return true;
+}
+
 function run() {
   const inputs = {
     githubToken: core.getInput("GITHUB_TOKEN", { required: true }),
@@ -14,37 +48,53 @@ function run() {
   if (!pr_number) {
     core.setFailed("No PR number available!");
   }
-  console.log(`1. PR Number is ${pr_number}\n`);
+  core.info(`PR Number is ${pr_number}`);
 
   const pr_body = github.context.payload.pull_request.body;
 
   if (!pr_body) {
     core.setFailed("No PR body available!");
   }
-  console.log(`2. PR Body: ${pr_body}`);
+  core.info(`PR Body: ${pr_body}`);
   const regex = /\[x\] If you want to rebase\/retry this PR, check this box/;
   const regexTest = new RegExp(regex);
   const isRebaseAllowed = regexTest.test(pr_body);
 
   if (!isRebaseAllowed) {
-    console.log("3. Rebase is not allowed since it is unchecked");
+    core.info("Rebase is not allowed since it is unchecked");
   } else {
-    console.log("3. Rebase is allowed, proceeding with the merge");
+    core.info("Rebase is allowed, proceeding with the merge");
 
-    const updatedBody = pr_body.replace(
-      regex,
-      `- [ ] If you want to rebase/retry this PR, check this box`
-    );
-    const params = {
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
-      pull_number: pr_number,
-      body: updatedBody,
-    };
     const octokit = github.getOctokit(inputs.githubToken);
-    octokit.pulls.update(params);
-    console.log("4. Updated the branch with unchecked body");
+    const prNeedsUpdateFlag = doesPrNeedsUpdate(octokit, pr_data);
+
+    if (prNeedsUpdateFlag) {
+      core.info("PR branch is behind master. Updating now....");
+      core.info(
+        "Rebase has been done successfully. Resetting the checkbox now..."
+      );
+      const updatedBody = pr_body.replace(
+        regex,
+        `- [ ] If you want to rebase/retry this PR, check this box`
+      );
+      const params = {
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        pull_number: pr_number,
+        body: updatedBody,
+      };
+      octokit.pulls.update(params);
+      core.info("Updated the branch with unchecked body");
+    } else {
+      core.warning(
+        "PR branch is up-to-date with master. Not proceeding with merge"
+      );
+    }
   }
 }
 
-run();
+try {
+  run();
+} catch (err) {
+  core.setFailed(err.message);
+}
