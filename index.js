afc4b47009fcfc3dd0cdf3b34c75bc930696ef3c
Srikar Samudrala <srikarsams@gmail.com>
2021-03-12 12:13:29 +0530

    update PR body

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(
-    github.context.payload.pull_request,
-    undefined,
-    2
+async function run() {
+  const inputs = {
+    githubToken: core.getInput("GITHUB_TOKEN", { required: true }),
+  };
+  const pr_data = github.context?.payload?.pull_request;
+  if (!pr_data) {
+    core.setFailed("No PR data available!");
+  }
+  const pr_number = github.context?.payload?.pull_request?.number;
+
+  if (!pr_number) {
+    core.setFailed("No PR number available!");
+  }
+  console.log(`1. PR Number is ${pr_number}\n`);
+
+  const pr_body = github.context?.payload?.pull_request?.body;
+
+  if (!pr_body) {
+    core.setFailed("No PR body available!\n");
+  }
+
+  const regex_for_rebase_check = new RegExp(
+    /\[x\] If you want to rebase\/retry this PR, check this box/
   );
-  console.log(`The event payload:  ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+  const isRebaseAllowed = regex_for_rebase_check.test(pr_body);
+
+  if (!isRebaseAllowed) {
+    console.log("2. Rebase is not allowed since it is unchecked\n");
+  } else {
+    console.log("2. Rebase is allowed, proceeding with the merge\n");
+    const params = {
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      pull_number: pr_number,
+      body: "[ ] If you want to rebase/retry this PR, check this box\n",
+    };
+    const octokit = new github.GitHub(inputs.githubToken);
+    await octokit.pulls.update(params);
+    console.log("3. Updated the branch with unchecked body\n");
+  }
 }
+
+run();
